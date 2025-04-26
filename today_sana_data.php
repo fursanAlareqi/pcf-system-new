56d2ce0d74173de1ae3a4a7c3eec85960cae6edf
Hudhaifa Al-sharabi <alsharabihudhaif@gmail.com>
2025-04-26 15:00:47 +0300

    edit all for sanaa and reception
    add page for edit all record for same code and branch

--- today_sana_data.php
+++ today_sana_data.php
@@ -139,6 +139,12 @@ if ($_SESSION['user']['rule_id'] == 23 || $_SESSION['user']['rule_id'] == 22 || 
                                                                     <a href="sana_edit.php?action=edit&ids=<?php echo $row['id'] ?>" >  
                                                                         تعديل
                                                                     </a>
+																	<?php if ($_SESSION['user']['rule_id'] == 2 || $_SESSION['user']['rule_id'] == 6){ ?>
+																	<a href="sana_edit_all.php?action=edit&ids=<?php echo $row['id'] ?>" >  
+                                                                        تعديل كل الادخالات
+                                                                    </a>
+																	<?php } ?>
+
 													            </td>
 																<?php
 															deleteAction($row['id']) ?>
